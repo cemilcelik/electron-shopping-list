90cd90ffc73aee8fabab8ea7259536c9c8829525
cemilcelik <cemilcelik84@gmail.com>
2017-12-21 17:58:46 +0300

    Initial commit.

---
+++ main.js
@@ -0,0 +1,96 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow,
+    addWindow;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function() {
+        app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+ipcMain.on('item:add', function(e, item) {
+    mainWindow.webContents.send('item:add', item);
+
+    addWindow.close();
+    addWindow = null;
+});
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                accelerator: 'Ctrl+N',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                accelerator: 'Ctrl+Shift+Delete',
+                click() {
+                    mainWindow.webContents.send('items:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darvin' ? 'Command+W' : 'Ctrl+W',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Developer Tools',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: 'F12',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    }
+]
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    addWindow.on('close', function() {
+        addWindow = null;
+    });
+}
